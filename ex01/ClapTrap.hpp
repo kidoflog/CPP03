d44ce21fc9207c9d130ee6a4b8643496cc2dfb88
kkido <kkido@student.42tokyo.jp>
2026-06-14 20:45:05 +0900

    ex01 tochuu

--- ex01/ClapTrap.hpp
+++ ex01/ClapTrap.hpp
@@ -6,13 +6,36 @@
 /*   By: kkido <kkido@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/14 12:51:02 by kkido             #+#    #+#             */
-/*   Updated: 2026/06/14 12:52:16 by kkido            ###   ########.fr       */
+/*   Updated: 2026/06/14 20:26:12 by kkido            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
 #define CLAPTRAP_HPP
 
+#include <iostream>
+#include <string>
 
+class ClapTrap {
+ public:
+  ClapTrap();
+  ClapTrap(const std::string& name);
+  ClapTrap(const std::string& name, const unsigned int new_hp,
+           const unsigned int new_ep, const unsigned int new_ad);
+  ClapTrap(const ClapTrap& src);
+  ClapTrap& operator=(const ClapTrap& src);
+  virtual ~ClapTrap();
+  virtual void attack(const std::string& target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
+
+ protected:
+  std::string _name;
+  unsigned int _hit_points;
+  unsigned int _energy_points;
+  unsigned int _attack_damage;
+  bool has_enough_hp();
+  bool has_enough_ep();
+};
 
 #endif
